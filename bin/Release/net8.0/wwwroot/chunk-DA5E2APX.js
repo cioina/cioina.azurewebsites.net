d1368d1a7282a19e4ce09aae184119eef9034781
cioina <alexei_cioina@yahoo.com>
2024-10-24 08:09:10 -0700

    update new version

---
+++ bin/Release/net8.0/wwwroot/chunk-DA5E2APX.js
@@ -0,0 +1,1 @@
+var e={version:"2024-10-23 11:20:26",hash:"d9470097754e98c875923974f972eaa9"};export{e as a};
--- bin/Release/net8.0/wwwroot/chunk-YNREUZBB.js
+++
@@ -1,1 +0,0 @@
-var f={version:"2024-10-21 12:32:45",hash:"0c909d4f561fa455f65bffb972d53063"};export{f as a};
